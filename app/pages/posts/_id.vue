297feb5202ea2a6421915518f83554a2b93868c4
Keisuke Shimotori <destrage1958@gmail.com>
2019-01-16 07:54:00 +0900

    completed

--- app/pages/posts/_id.vue
+++ app/pages/posts/_id.vue
@@ -7,6 +7,19 @@
           <small>by {{ post.created_at | time}}</small>
         </div>
         <p>{{ post.body }}</p>
+        <np-ssr>
+          <p class="text-right">
+            <el-button :disabled="!isLoggedIn" type="warning" v-if="isLiked"
+            @click="unlike" round>
+              <span class="el-icon-star-on" />
+              <span>{{post.likes.length}}</span>
+            </el-button>
+            <el-button :disabled="!isLoggedIn" type="warning" v-else @click="like" round>
+              <span class="el-icon-star-off" />
+              <span>{{ post.likes.length }}</span>
+            </el-button>
+          </p>
+        </np-ssr>
         <p class="text-right">{{ post.created_at | time}}</p>
       </el-card>
       <p>
@@ -19,26 +32,40 @@
 <script>
 import moment from '~/plugins/moment'
 import { mapGetters, mapActions } from 'vuex'
+import cloneDeep from 'lodash.clonedeep'
+
 export default {
   async asyncData({ store, route, error }) {
-    const { id } = route.params
-    if (store.getters['posts/posts'].find(p => p.id === id)) {
+    if (store.getters['posts/posts'].find(p => p.id === route.params.id)) {
       return
     }
-    try {
-      await store.dispatch('posts/fetchPost', { id })
-      if ( !( store.getters['posts/posts'].find(p => p.id === this.$route.params.id) ) ) {
-        throw new Error('post not found')
-      }
-    } catch(e) {
-      error({ statusCode: 404 })
-    }
+    await store.dispatch('posts/fetchPosts')
   },
   computed: {
     post() {
       return this.posts.find(p => p.id === this.$route.params.id)
     },
+    isLiked() {
+      if (!this.user) return false
+      return this.post.likes.find(l => l.user_id === this.user.id)
+    },
+    ...mapGetters(['user', 'isLoggedIn']),
     ...mapGetters('posts', ['posts'])
+  },
+  methods: {
+    like() {
+      if (!this.isLoggedIn) {
+        return
+      }
+      const likePayload = { user: this.user, post: this.post }
+      this.addLikeToPost(cloneDeep(likePayload))
+      this.addLikeLogToUser(cloneDeep(likePayload))
+    },
+    unlike() {
+
+    },
+    ...mapActions(['addLikeLogToUser']),
+    ...mapActions('posts', ['addLikeToPost'])
   },
   filters: {
     time(val) {
